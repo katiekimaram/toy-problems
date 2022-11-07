e28aeb10e3a4cf85b4ba090128f668ed707ae1b7
Katie Kim <24817268+katiekimaram@users.noreply.github.com>
2022-11-07 12:45:52 -0600

    Time: 102 ms (37.24%), Space: 42.2 MB (33.58%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    
+  let count = 0;
+  
+  while (num > 0) {
+      if (num % 2 === 1) {
+          num -= 1
+      } else {
+          num /= 2
+      }
+
+      count++;
+  }
+  
+  return count;
+  
+};
